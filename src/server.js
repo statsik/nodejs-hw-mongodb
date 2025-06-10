59db48ffc9ea387b98cbe38b10d0927282e6942f
Daniil Statsenko <daniilstatsenko23@gmail.com>
2025-06-10 12:55:36 +0200

    Commit-0

--- src/server.js
+++ src/server.js
@@ -36,7 +36,7 @@ export const setupServer = async () => {
         })
     })
 
-    app.get('/contact/:contactId', async (req, res) => {
+    app.get('/contacts/:contactId', async (req, res) => {
         const { contactId } = req.params;
         const contact = await getContactsById(contactId);
 
