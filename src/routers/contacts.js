2b71176bfa5021efcf083e6ac3fdae0009ce87a2
Daniil Statsenko <daniilstatsenko23@gmail.com>
2025-06-11 19:21:17 +0200

    hw-03-00

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createContactsController, editContactController, getAllContactsController, getContactsByIdController } from "../controllers/contacts.js";
+import { createContactsController, deleteContactController, editContactController, getAllContactsController, getContactsByIdController } from "../controllers/contacts.js";
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 
 const router = Router();
@@ -8,6 +8,6 @@ router.get('/contacts', ctrlWrapper(getAllContactsController));
 router.get('contacts/:contactId', ctrlWrapper(getContactsByIdController));
 router.post('/contacts', ctrlWrapper(createContactsController))
 router.put('/contacts/:contactId', ctrlWrapper(editContactController));
-router.delete('/contacts/:contactId', ctrlWrapper())
+router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController))
 
 export default router;
