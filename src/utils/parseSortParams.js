b6542e1b8bb63459833a30211858d56f0cda2480
Daniil Statsenko <daniilstatsenko23@gmail.com>
2025-06-25 10:42:12 +0200

    hw-04-0

--- src/utils/parseSortParams.js
+++ src/utils/parseSortParams.js
@@ -1,4 +1,4 @@
-import { SORT_ORDER } from "../constance.js";
+import { SORT_ORDER } from "../constance/index.js";
 
 const parseSortOrder = (sortOrder) => {
     const isKnownOrder = [SORT_ORDER.ASC, SORT_ORDER.DESC].includes(sortOrder);
