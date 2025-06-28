e3636cc776480e583241595e4bb6b5388fc3d29d
Daniil Statsenko <daniilstatsenko23@gmail.com>
2025-06-28 21:49:31 +0200

    Commit-0

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -1,28 +1,5 @@
 import createHttpError from "http-errors";
 
-// export const validateBody = (schema) => async(req, res, next) => {
-//     try {
-//         const validatedData = await schema.validate(req.body, {
-//             abortEarly: false,
-//             convert: false,
-//             allowUnknown: false,
-//         });
-//         req.body = validatedData;
-//         next();
-//     } catch (err) {
-//         const formattedErrors = err.details.map(detail => ({
-//             field: detail.path[0],
-//             message: detail.message,
-//             type: detail.type,
-//             context: detail.context
-//           }));
-//         const error = createHttpError(400, "Validation Error", {
-//             errors: formattedErrors,
-//         });
-//         next(error);
-//     }
-// }
-
 export const validateBody = (schema) => async (req, res, next) => {
     try {
         const validatedData = await schema.validateAsync(req.body, {
