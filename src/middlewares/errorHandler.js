7c2d8d6d5489eb010cc73c05460ae0d108bf6f12
Daniil Statsenko <daniilstatsenko23@gmail.com>
2025-06-28 20:41:55 +0200

    Commit-0

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,6 +1,18 @@
 import { HttpError } from 'http-errors';
 
 export const errorHandler = async (err, req, res, next) => {
+    if (err.isJoi){
+        return res.status(400).json({
+            status: 400,
+            message: "Something went wrong",
+            data: err.message,
+            details: err.details.map(({ path, message }) => ({
+                path,
+                message,
+            })),
+        });
+    }
+
     if (err instanceof HttpError) {
         return res.status(err.status).json({
             status: err.status,
