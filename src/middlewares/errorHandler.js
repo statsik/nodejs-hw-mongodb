8a21368667ce753ac1ff5e239fead6b81c97e125
Daniil Statsenko <daniilstatsenko23@gmail.com>
2025-06-28 19:38:59 +0200

    Commit-0

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -2,12 +2,11 @@ import { HttpError } from 'http-errors';
 
 export const errorHandler = async (err, req, res, next) => {
     if (err instanceof HttpError) {
-        res.status(err.status).json({
+        return res.status(err.status).json({
             status: err.status,
             message: err.message,
             data: err.message,
         });
-        return;
     }
 
     res.status(500).json({
