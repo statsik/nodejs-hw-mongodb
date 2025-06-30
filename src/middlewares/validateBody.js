4d6175da45d771c89f7bbf67dc2fa5f5881c247b
Daniil Statsenko <daniilstatsenko23@gmail.com>
2025-07-01 01:36:54 +0200

    Commit-0

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -18,7 +18,7 @@ export const validateBody = (schema) => async (req, res, next) => {
         next(createHttpError(400, {
             //message: "Validation Error",
             //errors: formattedErrors,
-            errors: err.details.message.replace(/"/g, '') + "\n",
+            errors: err.details.message.replace(/"/g, ''),
         }));
     }
 };
