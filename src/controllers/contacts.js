37b199cf2b57f9c9c4630c3df6cd9fd241d4bca5
Daniil Statsenko <daniilstatsenko23@gmail.com>
2025-07-05 01:14:41 +0200

    Commit-0

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -22,7 +22,7 @@ export const getAllContactsController = async (req, res) => {
     res.status(200).json({
         status: 200,
         message: "Successfully found contacts!",
-        data: contacts.data, 
+        data: contacts, 
     })
 }
 
