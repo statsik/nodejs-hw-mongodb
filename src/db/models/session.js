38a238942cb4d390f26e10a81fda3542141ca033
Daniil Statsenko <daniilstatsenko23@gmail.com>
2025-07-04 03:22:14 +0200

    Commit-0

--- src/db/models/session.js
+++ src/db/models/session.js
@@ -2,10 +2,10 @@ import mongoose, { model } from "mongoose";
 
 const sessionsSchema = new mongoose.Schema({
     userId: {
-        type: String,
-        required: true,
-        // type: mongoose.Schema.Types.ObjectId,
-        // ref: 'User',
+        // type: String,
+        // required: true,
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
     },
     accessToken: {
         type: String,
--- src/db/models/user.js
+++ src/db/models/user.js
@@ -15,10 +15,6 @@ const userSchema = new mongoose.Schema(
             type: String,
             required: true,
         },
-        userId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User',
-        }
     },
     {
         timestamps: true, 
