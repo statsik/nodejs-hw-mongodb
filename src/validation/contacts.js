40ac68067d82395cc11be7200bd6c33782928452
Daniil Statsenko <daniilstatsenko23@gmail.com>
2025-07-05 12:07:53 +0200

    Commit-0

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,5 +1,5 @@
 import Joi from 'joi';
-import { isValidObjectId } from 'mongoose';
+// import { isValidObjectId } from 'mongoose';
 
 export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
@@ -7,12 +7,12 @@ export const createContactSchema = Joi.object({
   email: Joi.string().email().min(3).max(20).required(),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().valid("work", "home", "personal").required(),
-  userId: Joi.string().custom((value, helper) => {
-    if (value && !isValidObjectId(value)) {
-      return helper.message('Parent id should be a valid mongo id');
-    }
-    return value;
-  })
+  // userId: Joi.string().custom((value, helper) => {
+  //   if (value && !isValidObjectId(value)) {
+  //     return helper.message('Parent id should be a valid mongo id');
+  //   }
+  //   return value;
+  // })
 });
 
 export const updateContactSchema = Joi.object({
