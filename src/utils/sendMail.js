3fa028c67c1549107db59ef14e0050895b91ab7b
Daniil Statsenko <daniilstatsenko23@gmail.com>
2025-07-06 16:01:18 +0200

    Commit-06-01

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -6,6 +6,7 @@ import { SMTP } from '../constance/index.js';
 const transporter = nodemailer.createTransport({
   host: getEnvVar(SMTP.SMTP_HOST),
   port: Number(getEnvVar(SMTP.SMTP_PORT)),
+  secure: true,
   auth: {
     user: getEnvVar(SMTP.SMTP_USER),
     pass: getEnvVar(SMTP.SMTP_PASSWORD),
