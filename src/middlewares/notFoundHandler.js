cfffa029ac765c11fb8311b0f7aae044f7cfdf25
Daniil Statsenko <daniilstatsenko23@gmail.com>
2025-07-06 13:59:37 +0200

    Commit-0

--- src/middlewares/notFoundHandler.js
+++ src/middlewares/notFoundHandler.js
@@ -1,7 +1,5 @@
 export const notFoundHandler = (req, res) => {
     res.status(404).json({
-        //message: "Contact not found",
-        message: "This is not found handler",
-
+        message: "Contact not found",
     })
 }
